062c09875b69426a42d0ef9c052771a52b40108d
Juan David Sanchez Mancilla <marko56_34@hotmail.com>
2021-11-10 00:26:48 -0500

    arreglando bug

--- src/main/java/com/mintic/misiontic/entity/Bike.java
+++ src/main/java/com/mintic/misiontic/entity/Bike.java
@@ -88,6 +88,22 @@ public class Bike {
 	public void setCategory(Category category) {
 		this.category = category;
 	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
 	
 	
 	
